03fac61f9403f5eef2e9bda82c810484804d923a
andiharun29 <andiharunalrasyid14@gmail.com>
2025-01-13 01:55:45 +0700

    Tambah fungsi ekstensi untuk konversi MahasiswaEvent ke model Mahasiswa

---
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.firebasepam.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebasepam.Model.Mahasiswa
+import com.example.firebasepam.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+
+fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jeniskelamin = jeniskelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
