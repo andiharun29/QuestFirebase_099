4801117abb8e2952ef79a912a750abafe2dfeb83
andiharun29 <andiharunalrasyid14@gmail.com>
2025-01-13 01:56:41 +0700

    Tambah data class MahasiswaEvent dengan properti default

--- app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebasepam/ui/viewmodel/InsertViewModel.kt
@@ -10,6 +10,16 @@ import com.example.firebasepam.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
 
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jeniskelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
